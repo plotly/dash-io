1de4bcf115bcff7f9e55cb3ac72efe963a4ad486
xhlulu <xhlperso@gmail.com>
2021-06-21 22:27:05 -0400

    Add dataframe tests

---
+++ tests/test_dataframes.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+import dash_io.mime as dim
+
+assert_fail_msg = "Original dataframe does not match decoded dataframe."
+
+
+df = pd.read_csv("tests/data/sample.csv")
+
+
+def test_csv_stringio():
+    encoded = dim.encode_pandas(
+        df, format="csv", mime_type="text", mime_subtype="csv", index=False
+    )
+    decoded = dim.decode_pandas(encoded, format="csv")
+
+    pd.testing.assert_frame_equal(df, decoded)
+
+
+def test_csv_bytesio():
+    encoded = dim.encode_pandas(df, format="csv", index=False)
+    decoded = dim.decode_pandas(encoded, format="csv")
+
+    pd.testing.assert_frame_equal(df, decoded)
+
+
+def test_excel():
+    encoded = dim.encode_pandas(df, format="xlsx", index=False)
+    decoded = dim.decode_pandas(encoded, format="xlsx")
+
+    pd.testing.assert_frame_equal(df, decoded)
+
+
+def test_parquet():
+    encoded = dim.encode_pandas(df, format="parquet")
+    decoded = dim.decode_pandas(encoded, format="parquet")
+
+    pd.testing.assert_frame_equal(df, decoded)
+
+
+def test_pickle():
+    encoded = dim.encode_pandas(df, format="pickle")
+    decoded = dim.decode_pandas(encoded, format="pickle")
+
+    pd.testing.assert_frame_equal(df, decoded)
