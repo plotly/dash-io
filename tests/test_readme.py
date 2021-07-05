784ee19fee3647c47d5c3ea7ff8dec8712bf7833
xhlulu <xhlperso@gmail.com>
2021-07-05 14:28:14 -0400

    Move output of help to another file, update tests

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -7,5 +7,5 @@ def test_readme():
     generated = pydoc.render_doc(dio, renderer=pydoc.plaintext)
     generated = generated.split("\nFILE\n")[0].strip()
 
-    with open("README.md", "r") as f:
+    with open("DOCS.txt", "r") as f:
         assert generated in f.read()
